1ecc5ff0372887f09372758af28d8884331f78fc
Zytka <zyta.slowianska@gmail.com>
2024-11-21 12:50:33 +0100

    progress: infinite loop while sorting

--- merge_sorted_array.c
+++ merge_sorted_array.c
@@ -1,9 +1,9 @@
 #include "leetcode.h"
 
+void	test_merge_sorted_array(void);
+
 static void	bubble_sort(int* a, int start, int size);
 static void	swap(int* a, int* b);
-
-void	test_merge_sorted_array(void);
 
 static void	merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n) {
 	(void) nums1Size;
@@ -11,27 +11,28 @@ static void	merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, i
 	int i = 0;
 	while (i < n)
 	{
-		nums1[m + i] = nums2[i];
+		if (m == 0)
+			nums1[i] = nums2[i];
+		else
+			nums1[n + i] = nums2[i];
 		i++;
 	}
-	bubble_sort(nums1, m - 1, m + n);
+	bubble_sort(nums1, n - 1, m);
 }
 
 void	bubble_sort(int* a, int start, int size)
 {
 	int	i;
-
 	i = start;
-	while (start > -1)
+	while (start > -2)
 	{
-		while (a[i] && i < size)
+		while (a[i] && i < size - 1)
 		{
 			if (a[i] > a[i+1])
 				swap(&a[i], &a[i+1]);
 			i++;
 		}
-		i = start;
-		start--;
+		i = 0;
 	}
 }
 
@@ -48,9 +49,18 @@ void	test_merge_sorted_array(void)
 {
 	int	m = 6;
 	int	n = 3;
+
 	int	a1[] = {4, 5, 6, 0, 0, 0};
 	int	a2[] = {1, 2, 3};
-
-	//TODO: debug to check for the while loops guardians
 	merge(a1, m, m, a2, n, n);
+	m = 0;
+	n = 1;
+	int	a3[] = {0};
+	int	a4[] = {1};
+	merge(a3, m, m, a4, n, n);
+	m = 3;
+	n = 3;
+	int a5[] = {1, 2, 3, 0, 0, 0};
+	int a6[] = {2, 5, 6};
+	merge(a5, m, m, a6, n, n);
 }
