dea993cac865751210113f5f2794e68b1ae7d4d3
Zytka <zyta.slowianska@gmail.com>
2024-11-24 22:37:34 +0100

    feat: (1) merge_sorted_array

--- merge_sorted_array.c
+++ merge_sorted_array.c
@@ -1,53 +1,32 @@
 #include "leetcode.h"
 
-void	test_merge_sorted_array(void);
-
-static void	bubble_sort(int* a, int start, int size);
-static void	swap(int* a, int* b);
-
 static void	merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n) {
-	(void) nums1Size;
-	(void) nums2Size;
 	int i = 0;
+	int a;
+	int z;
+	int j = m;
+	(void)nums1Size;
+	(void)nums2Size;
 	while (i < n)
 	{
-		if (m == 0)
-			nums1[i] = nums2[i];
-		else
-			nums1[n + i] = nums2[i];
+		a = 0;
+		while (a < j && nums2[i] > nums1[a])
+			a++;
+		z = m + n - 2;
+		while (z >= a)
+		{
+			nums1[z+1] = nums1[z];
+			z--;
+		}
+		nums1[z+1] = nums2[i];
+		j++;
 		i++;
 	}
-	bubble_sort(nums1, n - 1, m);
-}
-
-void	bubble_sort(int* a, int start, int size)
-{
-	int	i;
-	i = start;
-	while (start > -2)
-	{
-		while (a[i] && i < size - 1)
-		{
-			if (a[i] > a[i+1])
-				swap(&a[i], &a[i+1]);
-			i++;
-		}
-		i = 0;
-	}
-}
-
-void swap(int* a, int* b)
-{
-	int	tmp;
-
-	tmp = *a;
-	*a = *b;
-	*b = tmp;
 }
 
 void	test_merge_sorted_array(void)
 {
-	int	m = 6;
+	int	m = 3;
 	int	n = 3;
 
 	int	a1[] = {4, 5, 6, 0, 0, 0};
@@ -63,4 +42,9 @@ void	test_merge_sorted_array(void)
 	int a5[] = {1, 2, 3, 0, 0, 0};
 	int a6[] = {2, 5, 6};
 	merge(a5, m, m, a6, n, n);
+	m = 6;
+	n = 3;
+	int a7[] = {-1, 0, 0, 3, 3, 3, 0, 0, 0};
+	int a8[] = {1, 2, 2};
+	merge(a7, m, m, a8, n, n);
 }
